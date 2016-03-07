d17067a0d98fe8a126b3a683a498836d4248fc88
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-07 16:16:25 -0500

    added solution and passed test

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,13 +1,21 @@
-require "pry"
+# require "pry"
+class Monkey
+  attr_accessor :name, :species, :foods_eaten
+  def initialize(name, species)
+    @name = name
+    @species = species
+    @foods_eaten = []
+  end
+    def eat(food)
+      @foods_eaten << food
+      #@foods_eaten.push(food)
+    end
+    def introduce
+      "Hi! My name is #{@name} and I'm a big ol' #{@species} and I eat #{@foods_eaten.join(", and ")}!"
+    end
+end
 
-
-
-
-
-
-
-
-
-binding.pry
-
-puts "done."
+bob = Monkey.new("Bob","Chimp")
+bob.eat("pickles")
+bob.eat("more pickles")
+bob.eat("so many pickles")
